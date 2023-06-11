1f8df2fb879f39e01c8b93572ac7334516ab5006
Nikita <khakimov.nikita.vch@gmail.com>
2023-06-11 22:02:38 +0300

    customSelect

--- src/components/Controls/Search.jsx
+++ src/components/Controls/Search.jsx
@@ -5,19 +5,19 @@ import React from 'react';
 
 const InputContainer = styled.label`
   background-color: var(--colors-ui-base);
-	padding: 1rem 2rem;
-	display: flex;
-	align-items: center;
+  padding: 1rem 2rem;
+  display: flex;
+  align-items: center;
 
-	border-radius: var(--radius)
-	box-shadow: var(--shadow);
-	width: 100%;
-	margin-bottom: 1.5rem;
+  border-radius: var(--radius);
+  box-shadow: var(--shadow);
+  width: 100%;
+  margin-bottom: 1.5rem;
 
-	@media(min-width: 767px) {
-		margin-bottom: 0;
-		width: 280px;
-	}
+  @media (min-width: 767px) {
+    margin-bottom: 0;
+    width: 280px;
+  }
 `;
 const Input = styled.input.attrs({
   type: 'search',
